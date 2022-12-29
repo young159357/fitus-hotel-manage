927542042d934101b298c331e49aee440a6e0ac7
Vũ Huy Hoàng <vhhoang20@clc.fitus.edu.vn>
2022-12-29 21:02:59 +0700

    fix conflict

--- src/hotel_app/lib/main.dart
+++ src/hotel_app/lib/main.dart
@@ -4,6 +4,6 @@ import './loginScreen.dart';
 //import './Client.dart';
 //import './Admin.dart';
 //import './User.dart';
-import './Staff.dart';
+// import './Staff.dart';
 
 void main() => runApp(const Login());
