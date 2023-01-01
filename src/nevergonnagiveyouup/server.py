0945216bcf0fb4c05daa56c526ada9a2fe25e179
hoangbaoa <hoangbao1223@gmail.com>
2023-01-01 09:21:54 +0700

    server: Hotel info and moving some models

--- src/nevergonnagiveyouup/server.py
+++ src/nevergonnagiveyouup/server.py
@@ -14,29 +14,18 @@ class LoginInfo(BaseModel):
     password: str
 
 
-class UserInfo(BaseModel):
-    Name: str
-    Phone: str
-    Email: str
-    Gender: str
-    DOB: str
-    Address: str
-
-
 @app.post("/login", status_code=200)
 def login(login_info: LoginInfo, response: Response):
     if data.check_credentials(login_info.username, login_info.password):
-        user_data = data.get_user_info(login_info.username)
         return {
-            "user_info":
-            UserInfo(
-                **{
-                    key: user_data[i]
-                    for i, key in enumerate(UserInfo.__fields__.keys())
-                }),
-            "permissions":
-            data.get_user_permissions(login_info.username),
+            "user_info": data.get_user_info(login_info.username),
+            "permissions": data.get_user_permissions(login_info.username),
         }
     else:
         response.status_code = 401
         return
+
+
+@app.get("/hotels_info", status_code=200)
+def hotels_info():
+    return data.get_hotels_info()
